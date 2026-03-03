bcf538617271ac79de13913ec6617786b44d5112
PrabudasDevadoss <138124446+PrabudasDevadoss@users.noreply.github.com>
2026-03-03 10:31:18 +0530

    Add files via upload
    Let & If conditions

---
+++ Day02/learnIfCondition.js
@@ -0,0 +1,34 @@
+
+let num = 10
+if (num > 0)
+{console.log (num+" is Positive")}
+else if (num == 0)
+{console.log (num+" is Zero")}
+else
+{console.log (num+" is Negative")}
+
+
+
+/*let age = 15;
+if (age <= 18){    
+    console.log("is not elegible")
+}
+else { 
+    console.log("is elegible")
+}
+*/
+
+//else if (num < 0)
+//{    consol.log (num + " is Negative")}
+
+
+/*let income = 20;
+if (income<=8) {
+    console.log("no tax deducted")
+}else if(income>8 && income<15 ){
+   console.log("5% tax is deducted")
+}
+else{
+    console.log("tax deduceted is 20%")
+}
+    */
---
+++ Day02/learnLetKeyword.js
@@ -0,0 +1,10 @@
+//Check and print odd number between 1 to 20
+
+for(let num = 1; num <= 20; num++)
+{
+    if (num % 2 != 0)
+    {
+        console.log(num+ " is odd")
+    }
+
+ }
